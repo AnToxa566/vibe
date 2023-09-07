877693f3ae63558b869cde48d218adcf12eff50e
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-09-07 11:55:29 +0300

    + elipses on background at prices block

--- src/components/prices/prices.tsx
+++ src/components/prices/prices.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 import { AppSubtitle, AppTitle } from "~/common/enums/enums";
 import { BaseProps } from "~/common/interfaces/interfaces";
 import { Container, Title } from "../components";
 import { PricesTable } from "./components/components";
 
+import elipse from "~/../public/svg/elipse.svg";
+
 const Prices: React.FC<BaseProps> = ({ className = "" }) => {
   return (
     <div
-      className={`bg-regular-grey text-white rounded-tr-[6.875rem] rounded-bl-[6.875rem] pt-14 pb-32 overflow-hidden ${className}`}
+      className={`relative bg-regular-grey text-white rounded-tr-[6.875rem] rounded-bl-[6.875rem] pt-14 pb-32 overflow-hidden ${className}`}
     >
       <Container>
         <Title title={AppTitle.PRICES} white className="mb-10" />
@@ -17,6 +21,9 @@ const Prices: React.FC<BaseProps> = ({ className = "" }) => {
 
         <PricesTable />
       </Container>
+
+      <Image src={elipse} alt="" className="absolute right-1/2 top-10" />
+      <Image src={elipse} alt="" className="absolute left-3/4 bottom-2/4" />
     </div>
   );
 };
